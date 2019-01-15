5cda586cdf3e64081ff198088bcac0f6d7d3ab4e
Muhammad Naufal Rizqillah <komiknaufal2@gmail.com>
2019-01-16 00:58:36 +0700

    PR untuk hari pertama. Soal nomor 4-10

--- PRLogic01/PRLogic01/Program.cs
+++ PRLogic01/PRLogic01/Program.cs
@@ -10,7 +10,219 @@ namespace PRLogic01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int n = 9;
+            int i, j;
+            Console.WriteLine("Jawaban Soal Nomer 4");
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+
+                    if (j == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    else if (i == 0)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (i == j || i == 5 || j == 5 || j == (n + 1) - i)
+                    {
+                        Console.Write("* ");
+                    }
+
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Jawaban Soal Nomer 5");
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+
+                    if (j == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    else if (i == 0)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (i == j || i == 1 || j == 1 || i == n || j == n || j == (n + 1) - i)
+                    {
+                        Console.Write("* ");
+                    }
+
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Jawaban Soal Nomer 6");
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+
+                    if (j == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    else if (i == 0)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (i >= j)
+                    {
+                        Console.Write("* ");
+                    }
+
+
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Jawaban Soal Nomer 7");
+            for (i = n; i >= 0; i--)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+                    if (i == n)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (j == 0)
+                    {
+                        Console.Write(n - i + " ");
+                    }
+                    else if ((j - 1) >= (i))
+                    {
+                        Console.Write("* ");
+                    }
+
+
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Jawaban Soal Nomer 8");
+            for (i = n; i >= 0; i--)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+                    if (i == n)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (j == 0)
+                    {
+                        Console.Write(n - i + " ");
+                    }
+                    else if (i <= j - 1 && (n - i) >= j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if (i >= j - 1 && (n - i) <= j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Jawaban Soal Nomer 9");
+            for (i = n; i >= 0; i--)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+                    if (i == n)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (j == 0)
+                    {
+                        Console.Write(n - i + " ");
+                    }
+                    else if ((j - 1) <= i && (n - j) >= i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if ((j - 1) >= i && (n - j) <= i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Jawaban Soal Nomer 10");
+            for (i = n; i >= 0; i--)
+            {
+                for (j = 0; j <= n; j++)
+                {
+
+                    if (i == n)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    else if (j == 0)
+                    {
+                        Console.Write(n - i + " ");
+                    }
+                    else if ((j - 1) <= i && (n - j) >= i && (i - 3) != 0 && (i - 2) != 0 && (i - 1) != 0 && i != 0)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if ((j - 1) >= i && (n - j) <= i && (i + 4) != n && (i + 3) != n && (i + 2) != n && (i + 1) != n && i != n)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.Read();
         }
     }
 }
--- PRLogic01/PRLogic01/Properties/AssemblyInfo.cs
+++ PRLogic01/PRLogic01/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e4f9095-5fe4-4646-b328-2f2a3c262461")]
+[assembly: Guid("02519aea-fe02-4015-831e-0ba1e7b96356")]
 
 // Version information for an assembly consists of the following four values:
 //
