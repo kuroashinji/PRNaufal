46ad1152cccd902257ecc13ea72dbf6d1b0e07f2
Muhammad Naufal Rizqillah <komiknaufal2@gmail.com>
2019-01-28 08:41:10 +0700

    Revisi no 9

--- PRSql01/Sql1_DB_Mahasiswa_Soal9.sql
+++ PRSql01/Sql1_DB_Mahasiswa_Soal9.sql
@@ -1,10 +1,10 @@
 use DB_Mahasiswa_Soal
-CREATE VIEW tampil AS(
-SELECT Mahasiswa .Kode_Mahasiswa ,Mahasiswa .Nama_Mahasiswa ,Jurusan .Nama_Jurusan ,Agama .Deskripsi ,Dosen .Nama_Dosen ,Jurusan .Status_Jurusan 
+CREATE VIEW tampil2 AS(
+SELECT Mahasiswa.Kode_Mahasiswa, Mahasiswa.Nama_Mahasiswa, Jurusan.Nama_Jurusan, Agama.Deskripsi as Agama,  Dosen.Nama_Dosen, Jurusan.Status_Jurusan, Type_Dosen.Deskripsi
 FROM ((((Mahasiswa 
 INNER JOIN Jurusan ON Mahasiswa.Kode_Jurusan =Jurusan .Kode_Jurusan) 
 INNER JOIN Agama ON Mahasiswa .Kode_Agama =Agama .Kode_Agama ) 
 INNER JOIN Dosen ON Jurusan .Kode_Jurusan =Dosen .Kode_Jurusan )
 INNER JOIN Type_Dosen ON Dosen .Kode_Type_Dosen =Type_Dosen .Kode_Type_Dosen )
 WHERE Nama_Mahasiswa LIKE '%Budi%');
-SELECT * FROM tampil
+SELECT * FROM tampil2
